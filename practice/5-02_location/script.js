38a10d361546aff5dbdfdf82196e9aace2083398
takato15 <gshawks21@gmail.com>
2022-02-23 23:35:05 +0900

    [update]インデント修正

--- practice/5-02_location/script.js
+++ practice/5-02_location/script.js
@@ -2,14 +2,14 @@
 
 const lang = document.querySelector('html').lang;
 
-if(lang === 'ja') {
+if (lang === 'ja') {
     document.querySelector('option[value="index.html"]').selected = true;
-} else if(lang === 'en') {
+} else if (lang === 'en') {
     document.querySelector('option[value="index-en.html"]').selected = true;
-} else if(lang === 'zh') {
+} else if (lang === 'zh') {
     document.querySelector('option[value="index-zh.html"]').selected = true;
 }
 
-document.getElementById('form').select.onchange = function() {
+document.getElementById('form').select.onchange = function () {
     location.href = document.getElementById('form').select.value;
 }
